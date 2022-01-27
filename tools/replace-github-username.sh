9d6e7d4fd93f17ce57d2b1754b49bec1dae8f859
Alex <alexbrucely@gmail.com>
2022-01-26 17:17:28 -0800

    fix sed script for macos

--- tools/replace-github-username.sh
+++ tools/replace-github-username.sh
@@ -1,28 +1,30 @@
+# tested on macos
+
 #/bin/bash
-github_username="foo-user"
+github_username=''$1''
 
 # sed commands to replace github_username variable
 
 #platform-owners/cluster1
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster1/cluster1-apps.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster1/cluster1-cluster-config.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster1/cluster1-infra.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster1/cluster1-mesh-config.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster1/cluster1-apps.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster1/cluster1-cluster-config.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster1/cluster1-infra.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster1/cluster1-mesh-config.yaml
 
 #platform-owners/cluster2
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster2/cluster2-apps.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster2/cluster2-cluster-config.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster2/cluster2-infra.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster2/cluster2-mesh-config.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster2/cluster2-apps.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster2/cluster2-cluster-config.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster2/cluster2-infra.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster2/cluster2-mesh-config.yaml
 
 #platform-owners/cluster3
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster3/cluster3-apps.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster3/cluster3-cluster-config.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster3/cluster3-infra.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/cluster3/cluster3-mesh-config.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster3/cluster3-apps.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster3/cluster3-cluster-config.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster3/cluster3-infra.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/cluster3/cluster3-mesh-config.yaml
 
 #platform-owners/mgmt
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/mgmt/mgmt-apps.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/mgmt/mgmt-cluster-config.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/mgmt/mgmt-infra.yaml
-sed -i -e "s/ably77/${github_username}/g" ../platform-owners/mgmt/mgmt-mesh-config.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/mgmt/mgmt-apps.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/mgmt/mgmt-cluster-config.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/mgmt/mgmt-infra.yaml
+sed -i '' -e 's/ably77/'${github_username}'/g' ../platform-owners/mgmt/mgmt-mesh-config.yaml
