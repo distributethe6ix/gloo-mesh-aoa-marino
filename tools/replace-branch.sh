b0f5fb0a8147df184b823d652b652b6d1b10ac54
Alex <alexbrucely@gmail.com>
2022-01-27 12:50:48 -0800

    Create replace-branch.sh

---
+++ tools/replace-branch.sh
@@ -0,0 +1,37 @@
+# tested on macos
+#/bin/bash
+github_branch=''$1''
+
+# check to see if license key variable was passed through, if not prompt for key
+if [[ ${github_branch} == "" ]]
+  then
+    # provide license key
+    echo "Please provide the Github username used to fork this repo:"
+    read github_branch
+fi
+
+# sed commands to replace github_branch variable
+
+#platform-owners/cluster1
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster1/cluster1-apps.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster1/cluster1-cluster-config.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster1/cluster1-infra.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster1/cluster1-mesh-config.yaml
+
+#platform-owners/cluster2
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster2/cluster2-apps.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster2/cluster2-cluster-config.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster2/cluster2-infra.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster2/cluster2-mesh-config.yaml
+
+#platform-owners/cluster3
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster3/cluster3-apps.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster3/cluster3-cluster-config.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster3/cluster3-infra.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster3/cluster3-mesh-config.yaml
+
+#platform-owners/mgmt
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/mgmt/mgmt-apps.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/mgmt/mgmt-cluster-config.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/mgmt/mgmt-infra.yaml
+sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/mgmt/mgmt-mesh-config.yaml
