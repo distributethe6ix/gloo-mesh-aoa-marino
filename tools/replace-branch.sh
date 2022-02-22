1af9b207b01edb3941fafdd752b0e9cf9624fe43
Alex <alexbrucely@gmail.com>
2022-02-22 14:32:49 -0800

    Update replace-branch.sh

--- tools/replace-branch.sh
+++ tools/replace-branch.sh
@@ -1,37 +1,46 @@
 # tested on macos
 #/bin/bash
-github_branch=''$1''
+current_branch=''$1''
+target_branch=''$2''
 
-# check to see if github branch variable was passed through, if not prompt for it
-if [[ ${github_branch} == "" ]]
+# check to see if current branch variable was passed through, if not prompt for it
+if [[ ${current_branch} == "" ]]
+  then
+    # provide github branch
+    echo "Please provide the current targeted Github branch:"
+    read current_branch
+fi
+
+# check to see if target branch variable was passed through, if not prompt for it
+if [[ ${target_branch} == "" ]]
   then
     # provide github branch
     echo "Please provide the GitHub branch you want to use:"
-    read github_branch
+    read target_branch
 fi
 
-# sed commands to replace github_branch variable
+# sed commands to replace target_branch variable
 
 #platform-owners/cluster1
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster1/cluster1-apps.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster1/cluster1-cluster-config.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster1/cluster1-infra.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster1/cluster1-mesh-config.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster1/cluster1-apps.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster1/cluster1-cluster-config.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster1/cluster1-infra.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster1/cluster1-mesh-config.yaml
 
 #platform-owners/cluster2
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster2/cluster2-apps.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster2/cluster2-cluster-config.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster2/cluster2-infra.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster2/cluster2-mesh-config.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster2/cluster2-apps.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster2/cluster2-cluster-config.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster2/cluster2-infra.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster2/cluster2-mesh-config.yaml
 
 #platform-owners/cluster3
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster3/cluster3-apps.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster3/cluster3-cluster-config.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster3/cluster3-infra.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/cluster3/cluster3-mesh-config.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster3/cluster3-apps.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster3/cluster3-cluster-config.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster3/cluster3-infra.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/cluster3/cluster3-mesh-config.yaml
 
 #platform-owners/mgmt
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/mgmt/mgmt-apps.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/mgmt/mgmt-cluster-config.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/mgmt/mgmt-infra.yaml
-sed -i '' -e 's/HEAD/'${github_branch}'/g' ../platform-owners/mgmt/mgmt-mesh-config.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/mgmt/mgmt-apps.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/mgmt/mgmt-cluster-config.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/mgmt/mgmt-infra.yaml
+sed -i '' -e 's/'${current_branch}'/'${target_branch}'/g' ../platform-owners/mgmt/mgmt-mesh-config.yaml
