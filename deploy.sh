206a76d47b07d49d645f14ec933c5b9c8d3e4ee2
Alex <alexbrucely@gmail.com>
2022-04-23 11:52:44 -0700

    bump

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ cd ..
 ./tools/wait-for-rollout.sh deployment argocd-server argocd 20 ${cluster2_context}
 
 # deploy mgmt, cluster1, and cluster2 cluster config aoa
-#kubectl apply -f platform-owners/mgmt/mgmt-cluster-config.yaml --context ${mgmt_context}
+kubectl apply -f platform-owners/mgmt/mgmt-cluster-config.yaml --context ${mgmt_context}
 kubectl apply -f platform-owners/cluster1/cluster1-cluster-config.yaml --context ${cluster1_context}
 kubectl apply -f platform-owners/cluster2/cluster2-cluster-config.yaml --context ${cluster2_context}
 
