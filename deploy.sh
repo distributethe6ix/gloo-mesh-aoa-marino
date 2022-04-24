abe60cd4118d0aa9fd83f922b647a13107b0720c
Alex <alexbrucely@gmail.com>
2022-04-24 10:40:05 -0700

    set

--- deploy.sh
+++ deploy.sh
@@ -32,32 +32,36 @@ kubectl apply -f platform-owners/mgmt/mgmt-cluster-config.yaml --context ${mgmt_
 kubectl apply -f platform-owners/cluster1/cluster1-cluster-config.yaml --context ${cluster1_context}
 kubectl apply -f platform-owners/cluster2/cluster2-cluster-config.yaml --context ${cluster2_context}
 
-############# fix this later ####################
-
-# manual step because order matters - istio-base must come before istiod
-kubectl apply -f environments/cluster1/infra/active/istio-base.yaml --context ${cluster1_context}
-kubectl apply -f environments/cluster2/infra/active/istio-base.yaml --context ${cluster2_context}
-sleep 15
-
-# manual step because order matters - istiod must come before ingressgateway
-kubectl apply -f environments/cluster1/infra/active/istiod-${revision}.yaml --context ${cluster1_context}
-kubectl apply -f environments/cluster2/infra/active/istiod-${revision}.yaml --context ${cluster2_context}
-
-# wait for completion of istiod install
-./tools/wait-for-rollout.sh deployment istiod-${revision} istio-system 10 ${cluster1_context}
-./tools/wait-for-rollout.sh deployment istiod-${revision} istio-system 10 ${cluster2_context}
-
-# sleep 30
-echo sleeping for 30 seconds
-sleep 30
-
-############# fix this later ####################
+############## fix this later ####################
+#
+## manual step because order matters - istio-base must come before istiod
+#kubectl apply -f environments/cluster1/infra/active/istio-base.yaml --context ${cluster1_context}
+#kubectl apply -f environments/cluster2/infra/active/istio-base.yaml --context ${cluster2_context}
+#sleep 15
+#
+## manual step because order matters - istiod must come before ingressgateway
+#kubectl apply -f environments/cluster1/infra/active/istiod-${revision}.yaml --context ${cluster1_context}
+#kubectl apply -f environments/cluster2/infra/active/istiod-${revision}.yaml --context ${cluster2_context}
+#
+## wait for completion of istiod install
+#./tools/wait-for-rollout.sh deployment istiod-${revision} istio-system 10 ${cluster1_context}
+#./tools/wait-for-rollout.sh deployment istiod-${revision} istio-system 10 ${cluster2_context}
+#
+## sleep 30
+#echo sleeping for 30 seconds
+#sleep 30
+#
+############## fix this later ####################
 
 # this meta app will take over the manual apps above
 # deploy mgmt, cluster1, and cluster2 environment infra app-of-apps
 kubectl apply -f platform-owners/mgmt/mgmt-infra.yaml --context ${mgmt_context}
 kubectl apply -f platform-owners/cluster1/cluster1-infra.yaml --context ${cluster1_context}
 kubectl apply -f platform-owners/cluster2/cluster2-infra.yaml --context ${cluster2_context}
+
+# wait for completion of istiod install
+./tools/wait-for-rollout.sh deployment istiod-${revision} istio-system 10 ${cluster1_context}
+./tools/wait-for-rollout.sh deployment istiod-${revision} istio-system 10 ${cluster2_context}
 
 # wait for completion of gloo-mesh install
 ./tools/wait-for-rollout.sh deployment gloo-mesh-mgmt-server gloo-mesh 10 ${mgmt_context}
