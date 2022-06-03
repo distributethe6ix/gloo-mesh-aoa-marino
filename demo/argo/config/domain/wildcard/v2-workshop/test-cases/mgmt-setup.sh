91842e98b91b4ddd47523dab222b7eda76dedf7e
Alex <alexbrucely@gmail.com>
2022-06-03 11:51:40 -0700

    Update mgmt-setup.sh

--- demo/argo/config/domain/wildcard/v2-workshop/test-cases/mgmt-setup.sh
+++ demo/argo/config/domain/wildcard/v2-workshop/test-cases/mgmt-setup.sh
@@ -2,7 +2,11 @@
 
 mgmt_context="mgmt"
 
-kubectl apply -f ../mgmt/1.1.a.mgmt-root-trust.yaml --context ${mgmt_context}
+# newly generated self-signed root/intermediates
+#kubectl apply -f ../mgmt/1.1.a.mgmt-root-trust.yaml --context ${mgmt_context}
+
+# leverage existing root cert and generate intermediates from it
+kubectl apply -f ../mgmt/1.1.b.mgmt-root-trust-secretref.yaml --context ${mgmt_context}
 
 kubectl apply -f ../mgmt/1.2.a.mgmt-workspace.yaml --context ${mgmt_context}
 
