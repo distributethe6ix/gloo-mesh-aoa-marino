3d1ffc3fb783c5cb3b45874896bebbb80f9ae2c6
Alex <alexbrucely@gmail.com>
2022-06-03 09:59:17 -0700

    set

--- demo/argo/config/domain/wildcard/v2-workshop/test-cases/security-WAF.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cluster1_context="cluster1"
-
-kubectl apply -f ../cluster1/4.5.a-WAF-policy.yaml --context ${cluster1_context}
--- demo/argo/config/domain/wildcard/v2-workshop/test-cases/security-rate-limiting.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cluster1_context="cluster1"
-
-kubectl apply -f ../cluster1/4.4.a.security-rate-limiting.yaml --context ${cluster1_context}
